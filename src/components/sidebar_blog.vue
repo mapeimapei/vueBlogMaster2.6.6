ce7d4393a9974eb60fbc25e1fab2e82413b18007
9725029@qq.com <9725029@qq.com>
2020-04-07 23:25:32 +0800

    增加favicon

--- src/components/sidebar_blog.vue
+++ src/components/sidebar_blog.vue
@@ -1,40 +1,38 @@
 <template>
-    <ul>
-      <li v-for="(item,index) in menu" :key ="'menu'+index">
-          <router-link :to="item.path">{{item.name}}</router-link>
-      </li>
-    </ul>
+  <ul>
+    <li v-for="(item,index) in menu" :key="'menu'+index">
+      <router-link :to="item.path">{{item.name}}</router-link>
+    </li>
+  </ul>
 </template>
 <script>
 export default {
-  name: 'cSidebar',
-  props:[],
-  data(){
-    return{
-      menu:[
-              {
-                "name":"文章列表",
-                "path":"posts",
-              },
-              {
-                "name":"添加&编辑文章",
-                "path":"addSingle",
-              },
-              {
-                "name":"网络爬虫",
-                "path":"spider",
-              },
-            ]
-    }
+  name: "cSidebar",
+  props: [],
+  data() {
+    return {
+      menu: [
+        {
+          name: "文章列表",
+          path: "posts"
+        },
+        {
+          name: "添加&编辑文章",
+          path: "addSingle"
+        },
+        {
+          name: "网络爬虫",
+          path: "spider"
+        }
+      ]
+    };
   },
-  computed:{},
-  watch:{},
-  methods:{
-  },
-  mounted(){}
-}
+  computed: {},
+  watch: {},
+  methods: {},
+  mounted() {}
+};
 </script>
 
 <style scoped>
-
 </style>
